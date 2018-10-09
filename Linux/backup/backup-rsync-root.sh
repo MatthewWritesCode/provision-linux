7ca846b56ddc51778dbd6a43b3ee00de58e0927e
DevWright <DevWright@users.noreply.github.com>
2018-10-09 14:48:36 -0400

    rsync backup now uses exclude file.

--- Linux/backup/backup-rsync-root.sh
+++ Linux/backup/backup-rsync-root.sh
@@ -2,6 +2,4 @@
 # Where to backup to.
 dest="$1"
 
-bakup_files='--exclude={"/home/.ecryptfs","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /'
-
-rsync -aAXv $backup_files $dest
+rsync -av --exclude-from='rsync-excludes.txt' / $dest
